ad03310402732df78e94147e9a5892b93a818e8a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-11-28 13:07:56 +0530

    App.tsx

--- App.tsx
+++ App.tsx
@@ -4,14 +4,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { store } from './src/store/store';
 import RootNavigator from './src/navigation/RootNavigator';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <StatusBar style="auto" />
-        <RootNavigator />
-      </NavigationContainer>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <StatusBar style="light" />
+          <RootNavigator />
+        </NavigationContainer>
+      </View>
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F8F9FA',
+  },
+});
