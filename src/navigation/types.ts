b1d2784d91f391bca7e80ae665dabbb35581800d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-11-29 11:57:53 +0530

    navigation: types

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,13 +1,28 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
-    HomeTab: undefined;
-    TransactionsTab: undefined;
-    BanksTab: undefined;
-    ProfileTab: undefined;
+    Login: undefined;
+    OTP: { phoneNumber: string };
+    SetPin: { phoneNumber: string };
+    HomeMain: undefined;
     AddMoney: undefined;
     TransactionHistory: undefined;
     BankAccounts: undefined;
+    SendMoney: undefined;
+    RequestMoney: undefined;
+    QRScan: undefined;
+    QRGenerate: undefined;
+    PaymentConfirmation: {
+        receiverPhone: string;
+        receiverName: string;
+        amount: number;
+        description?: string;
+    };
+    PaymentSuccess: {
+        transactionId: string;
+        amount: number;
+        receiverName: string;
+    };
 };
 
 export type HomeStackParamList = {
@@ -15,21 +30,80 @@ export type HomeStackParamList = {
     AddMoney: undefined;
     TransactionHistory: undefined;
     BankAccounts: undefined;
+    SendMoney: undefined;
+    QRScan: undefined;
+    QRGenerate: undefined;
+    PaymentConfirmation: {
+        receiverPhone: string;
+        receiverName: string;
+        amount: number;
+        description?: string;
+    };
+    PaymentSuccess: {
+        transactionId: string;
+        amount: number;
+        receiverName: string;
+    };
 };
 
-export type TransactionsStackParamList = {
-    TransactionsMain: undefined;
+export type SendMoneyStackParamList = {
+    SendMoney: undefined;
+    PaymentConfirmation: {
+        receiverPhone: string;
+        receiverName: string;
+        amount: number;
+        description?: string;
+    };
+    PaymentSuccess: {
+        transactionId: string;
+        amount: number;
+        receiverName: string;
+    };
 };
 
-export type BanksStackParamList = {
-    BanksMain: undefined;
+export type QRScanStackParamList = {
+    QRScan: undefined;
+    PaymentConfirmation: {
+        receiverPhone: string;
+        receiverName: string;
+        amount: number;
+        description?: string;
+    };
+    SendMoney: undefined;
 };
 
-export type ProfileStackParamList = {
-    ProfileMain: undefined;
+export type PaymentConfirmationStackParamList = {
+    PaymentConfirmation: {
+        receiverPhone: string;
+        receiverName: string;
+        amount: number;
+        description?: string;
+    };
+    PaymentSuccess: {
+        transactionId: string;
+        amount: number;
+        receiverName: string;
+    };
+};
+
+export type PaymentSuccessStackParamList = {
+    PaymentSuccess: {
+        transactionId: string;
+        amount: number;
+        receiverName: string;
+    };
+    HomeMain: undefined;
+    TransactionHistory: undefined;
+    SendMoney: undefined;
+};
+
+export type QRGenerateStackParamList = {
+    QRGenerate: undefined;
 };
 
 export type HomeScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'HomeMain'>;
-export type AddMoneyScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'AddMoney'>;
-export type TransactionHistoryScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'TransactionHistory'>;
-export type BankAccountsScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'BankAccounts'>;
+export type SendMoneyScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'SendMoney'>;
+export type QRScanScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'QRScan'>;
+export type QRGenerateScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'QRGenerate'>;
+export type PaymentConfirmationScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'PaymentConfirmation'>;
+export type PaymentSuccessScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList, 'PaymentSuccess'>;
